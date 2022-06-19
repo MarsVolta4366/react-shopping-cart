82c3418b26113148ea85d76a245fcb7fa13545cd
unknown <derek.slauson5@gmail.com>
2022-06-19 14:49:25 -0500

    Created Item component, mapped data in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
-import { Badge, Drawer, Grid, LinearProgress } from "@material-ui/core"
-import { AddShoppingCart } from "@material-ui/icons"
 import { useState } from "react"
 import { useQuery } from "react-query"
+// Components
+import { Badge, Drawer, Grid, LinearProgress } from "@material-ui/core"
+import { AddShoppingCart } from "@material-ui/icons"
+import Item from "./Item/Item"
+// Styles
 import { Wrapper } from "./App.styles"
 
 // At 13:36min https://www.youtube.com/watch?v=sfmL6bGbiN8
@@ -26,10 +29,29 @@ const App = () => {
   const { data, isLoading, error } = useQuery<CartItemType[]>("products", getProducts)
   console.log(data)
 
+  const getTotalItems = () => null
+
+  const handleAddToCart = (clickedItem: CartItemType) => null
+
+  const handleRemoveFromCart = () => null
+
+  if (isLoading) {
+    return <LinearProgress />
+  }
+  if (error) {
+    return <div>Something went wrong...</div>
+  }
+
   return (
-    <div className="App">
-      Start
-    </div>
+    <Wrapper>
+      <Grid container spacing={3}>
+        {data?.map(item => (
+          <Grid item key={item.id} xs={12} sm={4}>
+            <Item item={item} handleAddToCart={handleAddToCart} />
+          </Grid>
+        ))}
+      </Grid>
+    </Wrapper>
   )
 }
 
