a35a59fa8cb95c8352b3a33d72a288ece3e1d9c2
unknown <derek.slauson5@gmail.com>
2022-06-19 14:11:25 -0500

    App is fetching data

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,31 @@
+import { Badge, Drawer, Grid, LinearProgress } from "@material-ui/core"
+import { AddShoppingCart } from "@material-ui/icons"
+import { useState } from "react"
+import { useQuery } from "react-query"
+import { Wrapper } from "./App.styles"
+
+// At 13:36min https://www.youtube.com/watch?v=sfmL6bGbiN8
+
+// Types
+export type CartItemType = {
+  id: number,
+  category: string,
+  description: string,
+  image: string,
+  price: number,
+  title: string
+  amount: number
+}
+
+const getProducts = async (): Promise<CartItemType[]> => {
+  return await (await fetch("https://fakestoreapi.com/products")).json()
+}
 
 const App = () => {
+
+  const { data, isLoading, error } = useQuery<CartItemType[]>("products", getProducts)
+  console.log(data)
+
   return (
     <div className="App">
       Start
